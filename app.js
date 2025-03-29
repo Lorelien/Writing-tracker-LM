2f503182736dc9b7894a27461df04050f997b7a3
Lorelien Michiels <r0894804@student.thomasmore.be>
2025-03-29 16:30:26 +0100

    app js toegevoegd

---
+++ app.js
@@ -0,0 +1,18 @@
+import Book from "./Classes/book"; 
+import Tracker from "./Classes/Tracker";
+import WritingLog from "./Classes/writingLog";
+
+const tracker = new Tracker();
+
+// Voeg boeken toe
+// tracker.addBook("Fantasy Avontuur", "Een episch verhaal.", "🌟");
+
+// Voeg schrijfsessies toe
+// tracker.addWritingLog("2025-03-01", "Fantasy Avontuur", 1500, 3);
+// tracker.addWritingLog("2025-03-02", "Mysterie Verhaal", 800, 1);
+
+// Bekijk maandelijkse statistieken
+// console.log(tracker.getMonthlyStats("2025-03"));
+
+// Vind het boek met de meeste woorden
+// console.log(tracker.getTopBook("2025-03"));
