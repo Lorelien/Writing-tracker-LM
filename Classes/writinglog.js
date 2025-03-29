65f6add6d209c72b63af4c1c4190f44f47b36f16
Lorelien Michiels <r0894804@student.thomasmore.be>
2025-03-29 16:18:06 +0100

    class writinglog

---
+++ Classes/writinglog.js
@@ -0,0 +1,8 @@
+export default class WritingLog {
+    constructor(date, book, words, chapters) {
+        this.date = date;
+        this.book = book;
+        this.words = words;
+        this.chapters = chapters;
+    }
+}
